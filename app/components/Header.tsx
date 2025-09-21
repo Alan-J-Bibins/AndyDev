d482f33cd8b269f010f6306e6df645e737de7968
Alan-J-Bibins <alanjbibins@gmail.com>
2025-09-22 03:47:17 +0530

    feat: Made the profile route and completed the breadcrumbs component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,11 +1,20 @@
+import { href, NavLink } from "react-router";
 import Breadcrumbs from "./Breadcrumbs";
 
 export default function Header() {
     return (
         <header className="px-8 py-4 flex justify-between items-center">
-            <h2 className="text-primary font-bold text-3xl">.niveD</h2>
-            <Breadcrumbs />
-            <div className="bg-primary rounded-full w-8 h-8" />
+            <div className="flex justify-start items-center gap-2">
+                <NavLink
+                    to={href('/')}
+                    className="text-primary font-bold text-3xl">
+                    .niveD
+                </NavLink>
+                <Breadcrumbs />
+            </div>
+            <NavLink to={href('/profile')}>
+                <div className="bg-primary rounded-full w-8 h-8" />
+            </NavLink>
         </header>
     )
 }
--- app/routes/_protected.project.$projectId.tsx
+++ app/routes/_protected.project.$projectId.tsx
@@ -19,6 +19,7 @@ export default function Page() {
     return (
         <main className="h-screen flex gap-4 p-1">
             <FileTree projectName={project.name} />
+            <CodeEditor />
         </main>
     );
 }
@@ -26,7 +27,15 @@ export default function Page() {
 export function FileTree({ projectName }: { projectName: string }) {
     return (
         <div className="bg-secondary/20 border border-primary/20 p-4">
-            <h3 className="text-primary uppercase tracking-widest">{projectName}</h3>
+            <h3 className="text-primary uppercase tracking-widest text-nowrap">{projectName}</h3>
+        </div>
+    );
+}
+
+export function CodeEditor() {
+    return (
+        <div className="bg-secondary/20 border border-primary/20 p-4 w-full">
+            <h3 className="text-primary uppercase tracking-widest">CODE EDITOR</h3>
         </div>
     );
 }
