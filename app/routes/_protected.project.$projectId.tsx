9452503f32b86fe26078f802da9a688fb12e412a
Alan-J-Bibins <alanjbibins@gmail.com>
2025-09-22 03:55:27 +0530

    feat: Setup layout for code editor page

--- app/routes/_protected.project.$projectId.tsx
+++ app/routes/_protected.project.$projectId.tsx
@@ -20,6 +20,10 @@ export default function Page() {
         <main className="h-screen flex gap-4 p-1">
             <FileTree projectName={project.name} />
             <CodeEditor />
+            <div className="flex flex-col gap-4 h-full w-full">
+                <SubagentStatus />
+                <ChatWithNived />
+            </div>
         </main>
     );
 }
@@ -36,6 +40,21 @@ export function CodeEditor() {
     return (
         <div className="bg-secondary/20 border border-primary/20 p-4 w-full">
             <h3 className="text-primary uppercase tracking-widest">CODE EDITOR</h3>
+        </div>
+    );
+}
+
+export function ChatWithNived() {
+    return (
+        <div className="bg-secondary/20 border border-primary/20 p-4 w-full h-full">
+            <h3 className="text-primary uppercase tracking-widest text-nowrap">CHAT WITH NIVED</h3>
+        </div>
+    );
+}
+export function SubagentStatus() {
+    return (
+        <div className="bg-secondary/20 border border-primary/20 p-4 w-full h-full">
+            <h3 className="text-primary uppercase tracking-widest text-nowrap">SUBAGENT STATUS</h3>
         </div>
     );
 }
